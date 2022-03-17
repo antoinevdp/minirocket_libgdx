1a5966e72d80a11293d31a496200f5f95960b2a5
antoinevdp <antoinevdp@live.fr>
2022-03-17 10:23:50 +0100

    fullscreen + target game

--- android/src/com/minirocket/game/AndroidLauncher.java
+++ android/src/com/minirocket/game/AndroidLauncher.java
@@ -1,6 +1,7 @@
 package com.minirocket.game;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Toast;
 
 import com.badlogic.gdx.backends.android.AndroidApplication;
@@ -13,7 +14,14 @@ public class AndroidLauncher extends AndroidApplication {
 		super.onCreate(savedInstanceState);
 		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
 		initialize(new main(), config);
-		Toast.makeText(AndroidLauncher.this, "Data has been saved successfully!", Toast.LENGTH_LONG).show();
+		getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_IMMERSIVE
+				| View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+				| View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+				| View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+				| View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+				| View.SYSTEM_UI_FLAG_FULLSCREEN
+				| View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
+		Toast.makeText(AndroidLauncher.this, "Test", Toast.LENGTH_LONG).show();
 	}
 
 
--- core/src/com/minirocket/game/main.java
+++ core/src/com/minirocket/game/main.java
@@ -7,36 +7,68 @@ import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.Intersector;
+import com.badlogic.gdx.math.Rectangle;
+import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.utils.ScreenUtils;
+
+import java.util.Random;
 
 public class main extends ApplicationAdapter {
 	SpriteBatch batch;
-	Texture img;
+	Texture targetTexture;
+	Rectangle targetRect;
+	Random rng;
+
+
 
 	BitmapFont font;
 	
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
-		//img = new Texture("badlogic.jpg");
+		targetTexture = new Texture("target.png");
+
+		targetRect = new Rectangle((Gdx.graphics.getWidth() / 2) - targetTexture.getWidth(), (Gdx.graphics.getHeight() / 2) - targetTexture.getHeight(), targetTexture.getWidth(), targetTexture.getHeight());
+
 		font = new BitmapFont();
 		font.setColor(Color.WHITE);
+
+		rng = new Random();
 	}
 
 	@Override
-	public void render () {
-		Gdx.gl.glClearColor(1,1,1,1);
+	public void render ()
+	{
+		//Clearing the background
+		Gdx.gl.glClearColor(0.5f, 0.5f, 1, 0.5f);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 
 		batch.begin();
-		font.getData().setScale(2.0f);
-		font.draw(batch,"Hello World",400,400);
+		batch.draw(targetTexture, targetRect.x, targetRect.y, targetRect.width, targetRect.height);
+
+		if(Gdx.input.justTouched())
+		{
+			Vector2 touchPosition = new Vector2(Gdx.input.getX(), Gdx.graphics.getHeight() - Gdx.input.getY());
+			Rectangle touchedRect = new Rectangle(touchPosition.x, touchPosition.y, 1, 1);
+			if(Intersector.overlaps(touchedRect, targetRect))
+			{
+				changeTargetPosition();
+			}
+		}
 		batch.end();
+	}
+
+	private void changeTargetPosition()
+	{
+		targetRect.setPosition(rng.nextInt(Gdx.graphics.getWidth() - (int) targetRect.width / 2) +
+				targetRect.height, rng.nextInt(Gdx.graphics.getHeight() - (int) targetRect.height / 2) +
+				targetRect.height);
 	}
 	
 	@Override
 	public void dispose () {
 		batch.dispose();
-		img.dispose();
+		targetTexture.dispose();
 	}
 }
